5a7f072d10010fa0a8a1f9dad4c78b8bee105818
ritenow-design <60059432+ritenow-design@users.noreply.github.com>
2022-06-28 12:21:56 +0530

    Update login.Test.js

--- Appium/Specs/login.Test.js
+++ Appium/Specs/login.Test.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 import LoginPage from '../pages/login.page'
-import { siteCredentials as credentials } from '../utils/utils'
+import { siteCredentials as credentials } from '..'
 
 describe('Login', () => {
   it('should login with the correct credentials', () => {
